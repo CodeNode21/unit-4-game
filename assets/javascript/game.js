e9aef5b9ade01e0802afd19cdd7d928a46c9123c
CodeNode21 <mbednara@gmail.com>
2019-05-12 22:36:12 -0400

    Styling added 75% complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// defining global variables below
+
+
+// function to generate random number
+
+// function to generate random numbers and assign to crystals
+
+// function to track score on score counter
+
+// function to determine if result matches score counter and note if won or lost
+
+// function updating wins and losses
+
+// function to set up new game
