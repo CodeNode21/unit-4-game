43716904a38b261ec1e71886141bfc8b06b4722c
CodeNode21 <mbednara@gmail.com>
2019-05-18 15:33:10 -0400

    Final push. JS Working as intended

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,11 +1,31 @@
-
+var randomResult;
 var wins = 0;
 var losses = 0;
 var score = 0;
 
+$("#wins").html("Wins: 0");
+$("#losses").html("Losses: 0");
 
+function resetGame(){
+    //seting randomNumber
+randomResult = Math.floor(Math.random() * 90) + 30;
+$("#random-number").html("Match this number: " + randomResult);
+console.log(randomResult);
 
+//setting values to crystals
+    var randNum1 = Math.floor(Math.random() * 11) + 1;
+    $("#data").html(randNum1);
+    var randNum2 = Math.floor(Math.random() * 11) + 1;
+    $("#crystal-2").html(randNum2);
+    var randNum3 = Math.floor(Math.random() * 11) + 1;
+    $("#crystal-3").html(randNum3);
+    var randNum4 = Math.floor(Math.random() * 11) + 1;
+    $("#crystal-4").html(randNum4);
 
+    score = 0;
+    $("#score-counter").html(score);
+
+};
 
 
 //seting randomNumber
@@ -30,33 +50,42 @@ $("#crystal-1").on("click", function () {
     score = score + randNum1;
     console.log(score, randNum1);
     $("#score-counter").html(score);
+    check();
 });
 $("#crystal-2").on("click", function () {
     score = score + randNum2;
     console.log(score, randNum2);
     $("#score-counter").html(score);
+    check();
 });
 $("#crystal-3").on("click", function () {
     score = score + randNum3;
     console.log(score, randNum3);
     $("#score-counter").html(score);
+    check();
 });
 $("#crystal-4").on("click", function () {
     score = score + randNum4;
     console.log(score, randNum4);
     $("#score-counter").html(score);
+    check();
 });
 
-
+function check(){
 if(score > randomResult){
     losses++;
-    console.log("You loose");
+    alert("You loose");
+    resetGame();
+    $("#losses").html("Losses: " + losses);
 }
 else if(score === randomResult){
     wins++;
-    console.log("Winner winner");
-}
+    alert("Winner winner");
+    resetGame();
+    $("#wins").html("Wins: " + wins);
+} 
 
+}
 
 
 //game with 4 crystals and random score between 50-120
